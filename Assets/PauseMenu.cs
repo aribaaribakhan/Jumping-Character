0fa1c81fc6c620cc240847928fb314f021be0feb
aribaaribakhan <aribaaribakhan0@gmail.com>
2023-08-02 10:45:40 +0500

    Initial commit

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause(bool pause)
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Reset()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Jumping-Character
+ There ia a character that jump over obstacles whea a space key pressed and game over when collied with obstacles.
